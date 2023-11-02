7657dbdaec1fc3f98d1659cb258a1d7a4a093e01
cantellinicole <cantelli.nicole@gmail.com>
2023-11-02 18:59:10 -0300

    implements retrofit

--- app/src/main/java/com/example/starwars_movies/network/ApiService.kt
+++ app/src/main/java/com/example/starwars_movies/network/ApiService.kt
@@ -1,16 +1,12 @@
 package com.example.starwars_movies.network
 
 import com.example.starwars_movies.feature.data.response.MoviesResponse
-import io.ktor.client.HttpClient
-import io.ktor.client.call.body
-import io.ktor.client.request.get
+import retrofit2.http.GET
 
-class ApiService {
+internal interface ApiService {
 
-    val client = HttpClient()
+    @GET("movies")
+    suspend fun getMovies() : Result<MoviesResponse>
 
-    internal suspend fun makeRequest(): MoviesResponse {
-        return client.get("https://api.npoint.io/dc12046175d1c54574fb").body()
-    }
 
 }
